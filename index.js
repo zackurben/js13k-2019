8372406d398fd9a7939582453eab9cc4ff49c681
Zack Urben <zackurben@gmail.com>
2019-08-20 19:29:17 -0500

    Updating code to do dynamic drawing of multiple tris.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const canvas = document.getElementById('c');
+const CACHE = {
+  _count: 20,
+  _last: [],
+  get() {
+    return this._last.reduce((sum, cur) => (sum += cur), 0) / this._count;
+  },
+  add(item) {
+    this._last.unshift(item);
+    this._last = this._last.slice(0, this._count);
+  }
+};
+const FPS = Object.create(CACHE);
+const DRAW = Object.create(CACHE);
+
+const canvas = document.querySelector('canvas');
+const fps = document.querySelector('div');
 const gl = canvas.getContext('webgl2');
 if (!gl) {
   console.error('no gl context');
@@ -38,55 +53,60 @@ const vertexShader = createShader(gl, gl.VERTEX_SHADER, vSource);
 const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fSource);
 const program = createProgram(gl, vertexShader, fragmentShader);
 
+//
 // MAIN
+//
 
-const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-const positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+// Our list of items to render
+const objs = [
+  [
+    // one
+    0,
+    0,
 
+    //two
+    0,
+    0.5,
 
-const colorLocation = gl.getUniformLocation(program, "u_color");
+    // three
+    0.7,
+    0
+  ],
+  [
+    // one
+    -0.9,
+    -0.9,
 
-// three 2d points
-const positions = [
-  // one
-  0,
-  0,
-  1,
+    //two
+    -0.7,
+    -0.1,
 
-  //two
-  0,
-  0.5,
-  1,
-
-  // three
-  0.7,
-  0,
-  1,
-
-  // second item
-  // one
-  -0.9,
-  -0.9,
-  0,
-
-  //two
-  -0.7,
-  -0.1,
-  0,
-
-  // three
-  -0.2,
-  -0.8,
-  0
+    // three
+    -0.2,
+    -0.8
+  ]
 ];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
+// Get all our shader attributes
+const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+// const colorLocation = gl.getUniformLocation(program, 'u_color');
+
+// Create our buffer
+const positionBuffer = gl.createBuffer();
+
+// Create our VAO for our new buffer
 const vao = gl.createVertexArray();
+
+// Bind our VAO
 gl.bindVertexArray(vao);
+
+// Enable our shader attribute
 gl.enableVertexAttribArray(positionAttributeLocation);
 
-const size = 3; // 3 components per iteration
+// Bind our rendering buffer to the current ARRAY_BUFFER
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+const size = 2; // 2 components per iteration
 const type = gl.FLOAT; // the data is 32bit floats
 const normalize = false; // don't normalize the data
 const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
@@ -100,42 +120,43 @@ gl.vertexAttribPointer(
   offset
 );
 
-// resize(gl.canvas);
-// gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-// // Clear the canvas
-// gl.clearColor(0, 0, 0, 1);
-// gl.clear(gl.COLOR_BUFFER_BIT);
-
-// Tell it to use our program (pair of shaders)
-gl.useProgram(program);
-
-// Bind the attribute/buffer set we want.
-gl.bindVertexArray(vao);
+// Define the viewport dimensions.
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
 let lastRender = 0;
-let now;
 let delta;
-render();
-
-function render(timestamp = 0) {
-  now = Date.now();
+(function render(timestamp = 0) {
   delta = timestamp - lastRender;
-  
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
   // Clear the canvas
   gl.clearColor(0, 0, 0, 1);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  const primitiveType = gl.TRIANGLES;
-  const offset = 0;
-  const count = 6;
-  gl.drawArrays(primitiveType, offset, count);
+  // Tell it to use our program (pair of shaders)
+  gl.useProgram(program);
 
-  lastRender = now;
+  // Bind the attribute/buffer set we want.
+  gl.bindVertexArray(vao);
+
+  // Render each of our objects
+  objs.forEach(obj => {
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(obj), gl.STATIC_DRAW);
+
+    const primitiveType = gl.TRIANGLES;
+    const offset = 0;
+    const count = 3;
+    gl.drawArrays(primitiveType, offset, obj.length / size);
+  });
+
+  if (fps) {
+    FPS.add(1000 / delta);
+    DRAW.add(delta);
+
+    fps.innerText = `frame ms: ${DRAW.get()}\nfps: ${FPS.get()}`;
+  }
+  lastRender = timestamp;
   return requestAnimationFrame(render);
-}
+})();
 
 function createShader(gl, type, source) {
   const shader = gl.createShader(type);
