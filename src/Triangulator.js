851f84073413bd508deff81394b02744b5106047
Zack Urben <zackurben@gmail.com>
2019-09-01 08:52:44 -0500

    Force adding files not being tracked by git, thanks vs code.

---
+++ src/Triangulator.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export default (faces, vertices) => {
+  return faces.map(face => {
+    const [a, b, c, d] = face;
+    return [
+      vertices[a], vertices[b], vertices[c],
+      vertices[a], vertices[c], vertices[d]
+    ].map(([x, y, z]) => [x, y, z]).flat();
+  });
+};
